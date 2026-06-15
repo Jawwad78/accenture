f70919c65f06578be48a52c1efbe94e219fc9762
jawwad78 <ja66730@gmail.com>
2026-06-15 19:35:28 +0100

    reformat: backend .tf with fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 #configure my s3 bucket so i can store my state file 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-jawwad"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
+    bucket       = "terraform-state-jawwad"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
     use_lockfile = true
   }
 }
