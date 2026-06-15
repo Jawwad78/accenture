49967cf81aac919521aaaeb710826a5e225af460
jawwad78 <ja66730@gmail.com>
2026-06-15 19:32:14 +0100

    add: native s3 lockfile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,9 @@
-#configure my s3 bucket so i can store my state file and enable dynamodb locking
+#configure my s3 bucket so i can store my state file 
 terraform {
   backend "s3" {
     bucket = "terraform-state-jawwad"
     key    = "terraform.tfstate"
     region = "eu-west-2"
-    # for dynamodb state locking ,we use  LockId in partition key
-    dynamodb_table = "terraform-state-lock-dynamo"
+    use_lockfile = true
   }
 }
-
-#lol, first time when I done it ^ I made them all variables, but variables weren't allowed 
